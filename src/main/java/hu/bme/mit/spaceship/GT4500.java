115405c623d7c78da3c57d2284fc0e53fe55f1a9
Karancsi Ádám <kari1393@sch.bme.hu>
2017-03-24 01:10:22 -0700

    meaningful change

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -77,6 +77,7 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
+    	  wasPrimaryFiredLast = false;
         if (! primaryTorpedoStore.isEmpty()) {
           firingSuccess = primaryTorpedoStore.fire(1);
           wasPrimaryFiredLast = true;
